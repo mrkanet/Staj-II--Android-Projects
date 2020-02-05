d792ebed9f5998c63517f2a9e1428e4bedd9a6e2
mrkanet <mrkaan888@yandex.com>
2020-02-05 21:00:57 +0300

    update sorting added bug fix for null text

--- 7.Retrofit/app/src/main/java/com/example/retrofit/network/ApiService.kt
+++ 7.Retrofit/app/src/main/java/com/example/retrofit/network/ApiService.kt
@@ -2,12 +2,21 @@ package com.example.retrofit.network
 
 import com.example.retrofit.ResponseModel
 import retrofit2.Call
+import retrofit2.http.Field
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
     @GET("search/repositories")
     fun getRepos(
         @Query("q") query: String
+    ): Call<ResponseModel>
+
+    @GET("search/repositories")
+    fun getSorted(
+        @Query("q") query: String,
+        @Query("order") order: String,
+        @Query("sort") sort: String
     ): Call<ResponseModel>
 }
--- 7.Retrofit/app/src/main/java/com/example/retrofit/ui/MainActivity.kt
+++ 7.Retrofit/app/src/main/java/com/example/retrofit/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.retrofit.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
@@ -15,35 +16,79 @@ import retrofit2.Call
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    @SuppressLint("DefaultLocale")
     override fun onClick(p0: View?) {
-        RetrofitClient.getClient()
-            .create(ApiService::class.java)
-            .getRepos(
-                search_input.text.toString()
-            )
-            .enqueue(object : retrofit2.Callback<ResponseModel> {
-                override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
-                    Log.d("Fail", t.message!!)
-                }
+        val lang = choose_language.selectedItem.toString()
+        var sortby = sort_by.selectedItem.toString()
+        lateinit var search: String
 
-                override fun onResponse(
-                    call: Call<ResponseModel>,
-                    response: Response<ResponseModel>
-                ) {
-                    val repos = response.body()
-                    rv_results.adapter = ResponseAdapter(
-                        repos!!
-                    ) {
-                        //onclick
-                        val open = Intent(Intent.ACTION_VIEW)
-                        open.data = Uri.parse("https://github.com/${it.owner.login}/${it.name}")
-                        startActivity(open)
-                    }
-                }
+        if (search_input.text.toString() != "") {
+            search = if (lang == "Any") search_input.text.toString()
+            else "${search_input.text}+lang:${lang}"
 
+            if (sortby == "Best Match") {
+                RetrofitClient.getClient()
+                    .create(ApiService::class.java)
+                    .getRepos(
+                        search
+                    )
+                    .enqueue(object : retrofit2.Callback<ResponseModel> {
+                        override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                            Log.d("Fail", t.message!!)
+                        }
 
+                        override fun onResponse(
+                            call: Call<ResponseModel>,
+                            response: Response<ResponseModel>
+                        ) {
+                            val repos = response.body()
+                            rv_results.adapter = ResponseAdapter(
+                                repos!!
+                            ) {
+                                //onclick
+                                val open = Intent(Intent.ACTION_VIEW)
+                                open.data =
+                                    Uri.parse("https://github.com/${it.owner.login}/${it.name}")
+                                startActivity(open)
+                            }
+                        }
+                    })
+            } else {
+                var sortas = sort_as.selectedItem.toString()
+                sortas = sortas.toLowerCase()
+                sortby = sortby.toLowerCase()
+                RetrofitClient.getClient()
+                    .create(ApiService::class.java)
+                    .getSorted(
+                        query = search,
+                        order = sortas,
+                        sort = sortby
+                    )
+                    .enqueue(object : retrofit2.Callback<ResponseModel> {
+                        override fun onFailure(call: Call<ResponseModel>, t: Throwable) {
+                            Log.d("Fail", t.message!!)
+                        }
 
-            })
+                        override fun onResponse(
+                            call: Call<ResponseModel>,
+                            response: Response<ResponseModel>
+                        ) {
+                            val repos = response.body()
+                            rv_results.adapter = ResponseAdapter(
+                                repos!!
+                            ) {
+                                //onclick
+                                val open = Intent(Intent.ACTION_VIEW)
+                                open.data =
+                                    Uri.parse("https://github.com/${it.owner.login}/${it.name}")
+                                startActivity(open)
+                            }
+                        }
+                    })
+
+            }
+        }
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
